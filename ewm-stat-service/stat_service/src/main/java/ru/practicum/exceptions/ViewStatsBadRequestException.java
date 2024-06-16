6b9c3eea004d638d2880f06f88aff5a42309e4a6
S.Petrovskii <sergdest261276@yandex.ru>
2024-06-16 13:47:48 +0300

    fix: main_svc Global refactoring 2

---
+++ ewm-stat-service/stat_service/src/main/java/ru/practicum/exceptions/ViewStatsBadRequestException.java
@@ -0,0 +1,7 @@
+package ru.practicum.exceptions;
+
+public class ViewStatsBadRequestException extends BadRequestException {
+    public ViewStatsBadRequestException(String message) {
+        super(message);
+    }
+}
