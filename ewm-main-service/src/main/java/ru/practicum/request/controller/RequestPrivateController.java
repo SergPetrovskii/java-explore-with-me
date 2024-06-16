b0a4b1e753387362792ec52246915230337fa614
S.Petrovskii <sergdest261276@yandex.ru>
2024-06-16 18:14:11 +0300

    fix: main_svc Revert last fix

--- ewm-main-service/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
+++ ewm-main-service/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.event.dto.NewEventDto;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.request.service.RequestServiceInterface;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -28,8 +26,7 @@ public class RequestPrivateController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ParticipationRequestDto save(@PathVariable Long userId, @RequestParam Long eventId,
-                                        @RequestBody @Valid NewEventDto dto) {
+    public ParticipationRequestDto save(@PathVariable Long userId, @RequestParam Long eventId) {
         log.info("Добавление запроса от текущего пользователя на участие в событии");
 
         return requestServiceInterface.save(userId, eventId);
