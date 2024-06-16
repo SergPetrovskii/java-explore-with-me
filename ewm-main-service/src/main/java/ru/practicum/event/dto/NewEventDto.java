10346b4a48507cb020a05f0309a984ff1f94cd66
S.Petrovskii <sergdest261276@yandex.ru>
2024-06-16 16:51:32 +0300

    fix: main_svc One more small fix

--- ewm-main-service/src/main/java/ru/practicum/event/dto/NewEventDto.java
+++ ewm-main-service/src/main/java/ru/practicum/event/dto/NewEventDto.java
@@ -9,12 +9,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import ru.practicum.location.model.Location;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -23,13 +23,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class NewEventDto {
     @NotBlank
-    @Length(min = 3, max = 120)
+    @Size(min = 3, max = 120, message = "Title must not be blank or empty and contain between 3 and 120 characters.")
     private String title;
     @NotBlank
-    @Length(min = 20, max = 2000)
+    @Size(min = 20, max = 2000, message = "Annotation must not be blank or empty and contain between 20 and 2000 characters.")
     private String annotation;
     @NotBlank
-    @Length(min = 20, max = 7000)
+    @Size(min = 20, max = 7000, message = "Description must not be blank or empty and contain between 20 and 7000 characters.")
     private String description;
     @NotNull
     @JsonSerialize(using = LocalDateTimeSerializer.class)
