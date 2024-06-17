4d7d6f9082115210588bc73bbd3872f8a4e97f62
S.Petrovskii <sergdest261276@yandex.ru>
2024-06-17 08:44:31 +0300

    fix: main_svc One more fix

--- ewm-main-service/src/main/java/ru/practicum/exceptions/ErrorHandler.java
+++ ewm-main-service/src/main/java/ru/practicum/exceptions/ErrorHandler.java
@@ -31,7 +31,7 @@ public class ErrorHandler {
     @ExceptionHandler({BadRequestException.class, NumberFormatException.class,
             MethodArgumentTypeMismatchException.class, MethodArgumentNotValidException.class,
             MissingServletRequestParameterException.class, ConstraintViolationException.class,
-            EventBadRequestException.class})
+            EventBadRequestException.class, ParticiopationBadRequestException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleBadRequestException(final BadRequestException exception) {
         return new ApiError(
